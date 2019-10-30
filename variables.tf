f205b1155f9454b5f18cef725fe4913e54c7a301
eziegenhorn <eric@ziegenhorns.com>
2019-10-30 14:12:17 -0500

    Declare asgs var type

--- variables.tf
+++ variables.tf
@@ -1,6 +1,10 @@
 variable "slack_webhook_url" {}
 variable "slack_channel" {}
-variable "asgs" {}
+
+variable "asgs" {
+  type        = "list"
+  description = "AWS ASGs to monitor"
+}
 
 variable "slack_username" {
   default = "AWS"
