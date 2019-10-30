2e553e0c1290df8513ecbe4f9ddf9b9d807e2429
eziegenhorn <eric@ziegenhorns.com>
2019-10-30 14:01:06 -0500

    Initial code

---
+++ main.tf
@@ -0,0 +1,31 @@
+module "notify_slack" {
+  source  = "terraform-aws-modules/notify-slack/aws"
+  version = "~> 1.0"
+
+  sns_topic_name = "slack-topic"
+
+  slack_webhook_url = "${var.slack_secret}"
+  slack_channel     = "it-sre-bot"
+  slack_username    = "AWS"
+}
+
+# Make cloudwatch alerts for specified ASGs 
+resource "aws_cloudwatch_metric_alarm" "asg_zero" {
+  count               = "${length(var.asgs)}"
+  alarm_name          = "testasg ASG has no instances"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  threshold           = "${var.minimum_instances}"
+  alarm_description   = "Alert if any AutoScalingGroup has no instances"
+  alarm_actions       = ["${module.notify_slack.this_slack_topic_arn}"]
+  ok_actions          = ["${module.notify_slack.this_slack_topic_arn}"]
+  actions_enabled     = true
+  metric_name         = "GroupTotalInstances"
+  namespace           = "AWS/AutoScaling"
+  period              = "60"
+  statistic           = "Sum"
+
+  dimensions {
+    AutoScalingGroupName = "${var.asgs[count.index]}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "slack_webhook_url" {}
+variable "slack_channel" {}
+variable "asgs" {}
+
+variable "slack_username" {
+  default = "AWS"
+}
+
+variable "minimum_instances" {
+  default = "0"
+}
